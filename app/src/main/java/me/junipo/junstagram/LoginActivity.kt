e9c97061a4878b99447fd57cbeec544a935cdd1a
이읍준 <66265293+junipo90@users.noreply.github.com>
2020-06-12 19:31:08 +0900

    firebase google login

--- app/src/main/java/me/junipo/junstagram/LoginActivity.kt
+++ app/src/main/java/me/junipo/junstagram/LoginActivity.kt
@@ -3,15 +3,26 @@ package me.junipo.junstagram
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     // firebase email 인증 변수
     var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +32,64 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
+        google_sign_in_button.setOnClickListener {
+            googleLogin()
+        }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
+    }
+
+    fun googleLogin() {
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+//            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+//                val account = task.getResult(ApiException::class.java)!!
+                var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+                var account = result?.signInAccount
+                Log.d("google_login", "firebaseAuthWithGoogle:" + account)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.d("google_login", "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("google_login", "signInWithCredential:success")
+                    moveMainPage(task.result?.user)
+//                    val user = auth?.currentUser
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.d("google_login", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    // ...
+//                    Snackbar.make(view, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+//                    updateUI(null)
+                }
+
+                // ...
+            }
     }
 
 
@@ -33,7 +101,7 @@ class LoginActivity : AppCompatActivity() {
                     moveMainPage(task.result?.user)
                 } else if (!task.exception?.message.isNullOrEmpty()) {
                     // Show the error message
-                    Toast.makeText(this, task.exception?.message,Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 } else {
                     // Login it you have account
                     signinEmail()
@@ -41,7 +109,7 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
-    fun signinEmail(){
+    fun signinEmail() {
         auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -49,13 +117,13 @@ class LoginActivity : AppCompatActivity() {
                     moveMainPage(task.result?.user)
                 } else {
                     // Show the error message
-                    Toast.makeText(this, task.exception?.message,Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 }
             }
     }
 
-    fun moveMainPage(user: FirebaseUser?){
-        if (user != null){
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
